18d5470c0956be2ec50e43984376402813953bd3
thewhuve <218629362+thewhuve@users.noreply.github.com>
2025-08-23 10:35:44 -0400

    Update RPS UI

--- javascript.js
+++ javascript.js
@@ -14,23 +14,29 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Enter rock, paper, or scissors:");
+    if (choice === null) {
+        console.log("Game cancelled.");
+        return null;
+    }
     return choice.toLowerCase();
 }
 
 function playRound(humanSelection, computerSelection) {
     if (
         (humanSelection === "rock" && computerSelection === "scissors") ||
-    (humanSelection === "paper" && computerSelection === "rock") ||
-    (humanSelection === "scissors" && computerSelection === "paper")) {
+        (humanSelection === "paper" && computerSelection === "rock") ||
+        (humanSelection === "scissors" && computerSelection === "paper")) {
         humanScore++;
         console.log("You win! " + humanSelection + " beats " + computerSelection + ".");
-    } else if (humanSelection === computerSelection) {
+    } 
+    else if (humanSelection === computerSelection) {
         console.log("It's a tie! You both chose " + humanSelection + ".");
-    } else {
+    } 
+    else {
         computerScore++;
         console.log("You lose! " + computerSelection + " beats " + humanSelection + ".");
     }
-    }
+}
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
@@ -38,6 +44,52 @@ function playGame() {
         let computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
         console.log("Score: You " + humanScore + " - " + computerScore + " Computer");
-    }}
+    }
+}
 
-playGame();
+const body = document.querySelector("body");
+const scoreDiv = document.createElement("div");
+scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+
+const rockbtn = document.createElement("button");
+rockbtn.innerText = "rock";
+const paperbtn = document.createElement("button");
+paperbtn.innerText = "paper";
+const scissorsbtn = document.createElement("button");
+scissorsbtn.innerText = "scissors";
+
+rockbtn.addEventListener("click", () => {
+    const computerSelection = getComputerChoice(); playRound("rock", computerSelection);
+    scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+    if (humanScore === 5) {
+        alert("You reached 5 points! You win the game!");
+        humanScore = 0;
+        computerScore = 0;
+        scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+    }
+});
+paperbtn.addEventListener("click", () => {
+    const computerSelection = getComputerChoice(); playRound("paper", computerSelection);
+    scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+    if (humanScore === 5) {
+        alert("You reached 5 points! You win the game!");
+        humanScore = 0;
+        computerScore = 0;
+        scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+    }
+});
+scissorsbtn.addEventListener("click", () => {
+    const computerSelection = getComputerChoice(); playRound("scissors", computerSelection);
+    scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+    if (humanScore === 5) {
+        alert("You reached 5 points! You win the game!");
+        humanScore = 0;
+        computerScore = 0;
+        scoreDiv.innerText = `Score: You ${humanScore} - ${computerScore} Computer`;
+    }
+});
+
+body.appendChild(rockbtn);
+body.appendChild(paperbtn);
+body.appendChild(scissorsbtn);
+body.appendChild(scoreDiv);
